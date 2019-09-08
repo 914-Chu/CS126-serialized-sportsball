b7cd43f879e5644161f0632492756c8f0ee1d8be
AlexMackowiak <Alex@Mackowiaks.com>
2019-09-08 16:56:42 -0500

    Initial commit

---
+++ test/DataTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DataTest {
+
+    @Test
+    public void canReadFileFromFolder() throws Exception {
+        String testFileContents = Data.getFileContents("test_resources", "test_file_contents.txt");
+        assertEquals("Gone, reduced to atoms", testFileContents);
+    }
+}
