34b58f07338f1ffd85ab904992e8392e6af6eb66
Joyce Chu <jujoyce12913804@gmail.com>
2019-09-09 14:25:16 -0500

    version 9: deleted Teams, Players, Goals, and Passes classes

--- src/MAIN.java
+++ src/MAIN.java
@@ -5,25 +5,9 @@ public class MAIN {
 
     public static void main(String[] args) {
 
-        //serialize();
          deserialize();
     }
-/**
-    private static void serialize() {
 
-        Players p1 = new Players("Joyce", 21, 1, "none", "Taiwan");
-        Players p2 = new Players( "Mercedes Jackson", 24, 13, "Midfielder", "Urbana");
-        Players p3 = new Players("William Glass", 21, 16, "Midfielder", "Champaign");
-
-        List<Players> hT = Arrays.asList(p1, p2, p3);
-        Teams homeTeam = new Teams("Champaign United", hT);
-        Game g1 = new Game(homeTeam);
-
-        Gson gson = new Gson();
-        String json = gson.toJson(g1);
-
-    }
-*/
     private static void deserialize() {
 
         String game1 = "{\n" +
